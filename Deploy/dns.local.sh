bb156727c1ac43023ab9c9bc5a0015eb358edd3c
TamVo <Tam.VoMinh212@gmail.com>
2020-01-12 13:08:02 +0700

    Kibana dns (#5)
    * Add & Map kibana
    
    * Remove port mapping on localhost:5601

--- Deploy/dns.local.sh
+++ Deploy/dns.local.sh
@@ -2,3 +2,4 @@
 sed -i '127.0.0.1  nmro.local'          /etc/hosts 
 sed -i '127.0.0.1  oidc.nmro.local'     /etc/hosts
 sed -i '127.0.0.1  api.nmro.local'      /etc/hosts
+sed -i '127.0.0.1  kibana.nmro.local'   /etc/hosts
