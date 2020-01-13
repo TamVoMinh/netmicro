81c496621dc5d5899a6d1cafc643f063cbeae90c
TamVo <Tam.VoMinh212@gmail.com>
2020-01-13 08:48:05 +0700

    Fix api-gateway logging (#6)

--- src/Gateways/ApiGateway/Startup.cs
+++ src/Gateways/ApiGateway/Startup.cs
@@ -1,24 +1,36 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nmro.ApiGateway.Extentions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 namespace Nmro.ApiGateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+         public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {       
+            services.AddLogging(logging => {
+                logging.ClearProviders();
+                logging.AddSerilog(dispose: true);
+            });
+                 
             services.AddOcelot();
             services.AddHealthChecks();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
