ac9fe70301730a8391e3fc1432c2c43d3fa639bb
Tam Vo <minhtam.it212@gmail.com>
2020-02-09 15:56:27 +0700

    Add DevOnly postgres datatabase & demo ctx

---
+++ Services/IAM/Migrations/20200209084521_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Nmro.IAM.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IdentityUsers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedDate = table.Column<DateTime>(nullable: true),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: true),
+                    UpdatedBy = table.Column<long>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    LastSuccessfulLogin = table.Column<DateTime>(nullable: false),
+                    LastFailedLogin = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdentityUsers");
+        }
+    }
+}
---
+++ Services/IAM/Models/IdentityUserModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Nmro.IAM.Models
+ {
+ public class IdentityUserModel
+    {
+        public string UserName {get;set;}
+
+        public string Password {get;set;}
+
+        public string Email {get;set;}
+    }
+ }
