af81a39ccf70bb0fb940d3d96ab20decd77d47ec
TamVo <Tam.VoMinh212@gmail.com>
2020-02-14 14:24:34 +0700

    Swagger UI integration (#29)
    * Add Swagger ui tool
    
    *  Intergrate with docker

--- Scripts/dns.local.sh
+++ Scripts/dns.local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sed -i '127.0.0.1  nmro.local'          /etc/hosts 
+sed -i '127.0.0.1  nmro.local'          /etc/hosts
 sed -i '127.0.0.1  oidc.nmro.local'     /etc/hosts
 sed -i '127.0.0.1  api.nmro.local'      /etc/hosts
 sed -i '127.0.0.1  kibana.nmro.local'   /etc/hosts
+sed -i '127.0.0.1  docs.nmro.local'   /etc/hosts
