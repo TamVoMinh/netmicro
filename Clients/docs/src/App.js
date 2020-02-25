e6c772287306c1835d132b43d3c8e87bd2639bdc
TamVo <Tam.VoMinh212@gmail.com>
2020-02-25 21:32:02 +0700

    clean apigateway log (#58)

--- Clients/docs/src/App.js
+++ Clients/docs/src/App.js
@@ -5,9 +5,7 @@ import "swagger-ui-react/swagger-ui.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <SwaggerUI url="http://api.nmro.local/iam/oas/v1/swagger.json/" />
-      </header>
+      <SwaggerUI url="http://api.nmro.local/iam/oas/v1/swagger.json/" />
     </div>
   );
 }
