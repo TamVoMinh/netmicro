d4ff50b034fb7329dab1bdb331b48c53c4ff4492
htduy262 <htduy262@gmail.com>
2020-02-29 10:59:34 +0700

    Add client reactjs - login with oidc implicit flow (#61)
    * Update code for testing
    
    * Done reactjs implicit flow - locally
    
    * Rename
    
    * Update for docker

---
+++ Clients/engage/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import AppContent from './components/AppContent';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'toastr/build/toastr.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppContent />
+    </div>
+  );
+}
+
+export default App;
