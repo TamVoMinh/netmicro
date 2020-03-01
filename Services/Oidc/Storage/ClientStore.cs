ab6a6126a27b224a8599ecbd5d524a5127ff621e
Tam Vo <minhtam.it212@gmail.com>
2020-03-01 16:59:30 +0700

    Add logger for debuging

--- Services/Oidc/Storage/ClientStore.cs
+++ Services/Oidc/Storage/ClientStore.cs
@@ -1,5 +1,6 @@
 using IdentityServer4.Models;
 using IdentityServer4.Stores;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Nmro.Oidc.Infrastructure;
 using System.Net.Http;
@@ -11,12 +12,16 @@ namespace Nmro.Oidc.Storage
     {
         private readonly IHttpClientFactory _clientFactory;
         private readonly HttpClient iamClient;
+        private readonly ILogger<ClientStore> _logger;
 
-        public ClientStore(IHttpClientFactory clientFactory)
+
+        public ClientStore(IHttpClientFactory clientFactory, ILogger<ClientStore> logger)
         {
             _clientFactory = clientFactory;
 
             iamClient = clientFactory.CreateClient("iam");
+
+            _logger = logger;
         }
 
         public async Task<Client> FindClientByIdAsync(string clientId)
@@ -27,7 +32,11 @@ namespace Nmro.Oidc.Storage
 
             var responseString = await response.Content.ReadAsStringAsync();
 
+            _logger.LogInformation(" RESPONSE_CLIENT: {responseString}", responseString);
+
             var client = JsonConvert.DeserializeObject<Client>(responseString);
+
+            _logger.LogInformation("Parsed Model:  {@Client}", client);
 
             return client;
         }
