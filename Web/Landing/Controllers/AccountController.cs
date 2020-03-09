299d47d635408e65a3b84d4f5e45dfe180fd4f45
tungngoc <tungngocitus@gmail.com>
2020-03-09 14:56:51 +0700

    removed unused namespaces

--- Web/Landing/Controllers/AccountController.cs
+++ Web/Landing/Controllers/AccountController.cs
@@ -5,10 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Nmro.Landing.Controllers
