866df8f8881aaaaa022a92877a2d1b084c6159a9
TuanTran <tuantranlhu@gmail.com>
2020-03-12 18:33:08 +0700

    add redis dockerfile

--- DevOps/up.sh
+++ DevOps/up.sh
@@ -1,2 +1,3 @@
  #!/bin/sh
- docker-compose -f docker-compose.override.yml -f Backbone/docker-compose.yml -f DevOnly/docker-compose.yml -f Foundation/Elk/docker-compose.yml -f Services/docker-compose.yml -f Web/docker-compose.yml -f Clients/docker-compose.yml up -d
+ echo "options"  $1
+ docker-compose -f docker-compose.override.yml -f Backbone/docker-compose.yml -f DevOnly/docker-compose.yml -f Foundation/Elk/docker-compose.yml -f Services/docker-compose.yml -f Web/docker-compose.yml -f Clients/docker-compose.yml up -d $1
