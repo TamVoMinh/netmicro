f329cf2e28329470e58c86c8c2d142b64f58e73d
TamVo <Tam.VoMinh212@gmail.com>
2020-03-25 18:46:28 +0700

    Add simple authz 2 apigatway (#121)
    * Support running local
    
    * Add  jwtBearer authentication to apigatway
    
    Co-authored-by: Tam Vo Minh <tam.vo@orientsoftware.com>

--- Clients/docs/src/App.js
+++ Clients/docs/src/App.js
@@ -5,7 +5,7 @@ import "swagger-ui-react/swagger-ui.css"
 function App() {
   return (
     <div className="App">
-      <SwaggerUI url="http://api.nmro.local/iam/oas/v1/swagger.json/" />
+      <SwaggerUI url="http://api.nmro.local/oas/v1/swagger.json" />
     </div>
   );
 }
