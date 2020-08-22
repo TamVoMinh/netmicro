296f9fd5051bb2b25b44b21df36ff08c00b1630e
TamVo <Tam.VoMinh212@gmail.com>
2020-08-22 10:23:57 +0700

    Correct path to cli scripts

--- devops/cli/dcb-list.sh
+++ devops/cli/dcb-list.sh
@@ -3,9 +3,9 @@ _COMPOSES=(
     -f docker-compose.yml
     -f foundation/backbone/docker-compose.yml
     -f foundation/backings/docker-compose.yml
-    -f capacities/security/docker-compose.yml
-    -f capacities/portal/docker-compose.yml
-    -f capacities/clients/docker-compose.yml
+    -f business/security/docker-compose.yml
+    -f business/portal/docker-compose.yml
+    -f business/clients/docker-compose.yml
 )
 
 
