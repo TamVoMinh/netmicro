30bc7acc781c94d88d3d408578d6966fca65993e
TamVo <Tam.VoMinh212@gmail.com>
2020-08-26 09:24:41 +0700

    Refactor naming & migration

--- business/security/IAM/Faces/Worker/Program.cs
+++ business/security/IAM/Faces/Worker/Program.cs
@@ -4,7 +4,7 @@ namespace Nmro.Security.IAM.Faces.Worker
 {
     public class Program
     {
-        public static readonly string AppName = "iam-async-worker";
+        public static readonly string AppName = "iam-worker";
 
         public static int Main(string[] args) => NmroWebHost.Build<Startup>(args);
     }
